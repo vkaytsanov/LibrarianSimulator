deb3f4f0ec2299b83ab461ea62a215b52ec70479
vkaytsanov <vkaytsanov@yahoo.com>
2021-05-02 14:12:34 +0300

    better ui, bodyguard sfx

--- Assets/Scripts/Bodyguard/Bodyguard.cs
+++ Assets/Scripts/Bodyguard/Bodyguard.cs
@@ -7,11 +7,13 @@ namespace Bodyguard {
 
 	public class Bodyguard : MonoBehaviour {
 		private Animator _animator;
+		private AudioSource _audioSource;
 		private const float MovingSpeed = 2.5f;
 		private bool _isLeaving = false;
 		private bool _isMoving = false;
 		private bool _isAttacking = false;
 
+		private bool _didAttacked = false;
 		private float _currentAttackingTime = 0.0f;
 		private const float AttackingTime = 4.0f;
 		private bool _didKnockDownStudent = false;
@@ -21,6 +23,7 @@ namespace Bodyguard {
 		// Start is called before the first frame update
 		void Start() {
 			_animator = GetComponent<Animator>();
+			_audioSource = GetComponent<AudioSource>();
 		}
 
 		// Update is called once per frame
@@ -34,6 +37,10 @@ namespace Bodyguard {
 
 			if (_isAttacking) {
 				_currentAttackingTime += Time.deltaTime;
+				if (!_didAttacked) {
+					_audioSource.Play();
+					_didAttacked = true;
+				}
 				
 				if (_currentAttackingTime > AttackingTime / 2) {
 					_didKnockDownStudent = NPCManager.Instance.npcComponent.IsKnockedDown();
@@ -51,6 +58,7 @@ namespace Bodyguard {
 			_isLeaving = true;
 			_isAttacking = false;
 			_currentAttackingTime = 0.0f;
+			_didAttacked = false;
 		}
 		
 		public void SetToComing() {
